2aedfff9d830a47cccf62a249e1afe7ce16f2e1d
mj862 <marjajkurkela@gmail.com>
2018-09-22 21:46:08 +0300

    Add project files.

---
+++ Yksikkötestausharjoitus/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yksikkötestausharjoitus
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hetu ="041085-072W";
+            
+            Console.WriteLine(HeTuOk(hetu));
+        }
+
+        public static bool HeTuOk(string hetu)
+        {
+            string pvm;
+            char välimerkki;
+            string numero;
+            char tarkistusmerkki;
+            string tMerkit = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+
+            hetu = hetu.Trim();
+
+            if (hetu.Length != 11) { return false; }
+
+            pvm = hetu.Substring(0, 6);
+            välimerkki = hetu[6];
+            numero = hetu.Substring(7, 3);
+            tarkistusmerkki = hetu[10];
+
+            int i = 0;
+            if (!int.TryParse(numero, out i)) { return false; }
+
+            if (välimerkki != '-' && välimerkki != '+' && välimerkki != 'A')
+            {
+                return false;
+            }
+
+            if (!int.TryParse(numero, out i)) { return false; }
+            i = int.Parse(pvm + numero) % 31;
+            if (tMerkit[i] != tarkistusmerkki)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ YksikkötestausharjoitusTests/ProgramTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yksikkötestausharjoitus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Yksikkötestausharjoitus.Program;
+
+namespace Yksikkötestausharjoitus.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void HeTuOkTest()
+        {            
+            string hetu = "121288-1233";
+            bool expected = true;
+            bool actual = HeTuOk(hetu);
+            Assert.AreEqual(expected, actual, "Joku pielessä, pitäisi olla ok hetu");
+        }
+        [TestMethod()]
+        public void HeTuOkTest2()
+        {
+            string hetu = "120177-1A22";
+            bool expected = false;
+            bool actual = HeTuOk(hetu);
+            Assert.AreEqual(expected, actual, "Joku pielessä, pitäisi olla ok hetu");
+        }
+    }
+}
